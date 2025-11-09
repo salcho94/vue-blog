ffafa6d03c963edeb18233f75ab71e557e998f0b
DESKTOP-4D2CR2T\RADCNS <kim_jisoup@radcns.com>
2025-11-10 08:34:26 +0900

    build 에러 수정 진행

--- vite.config.ts
+++ vite.config.ts
@@ -1,13 +1,15 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
-import { resolve } from 'path'
+import { fileURLToPath, URL } from 'node:url'
 
 export default defineConfig({
   plugins: [vue()],
   resolve: {
     alias: {
-      '@': resolve(__dirname, './src'),
+      '@': fileURLToPath(new URL('./src', import.meta.url)),
     },
   },
-  base: '/vue-blog/', //
+  // GitHub Pages 리포지토리명에 맞게 수정
+  // 루트 도메인에 올리면 '/' 로 바꾸면 됨
+  base: '/salcho-blog/',
 })
