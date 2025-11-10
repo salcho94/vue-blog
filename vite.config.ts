aafe3ac7684de3d4913dbe1b338b0a96d02165dd
DESKTOP-4D2CR2T\RADCNS <kim_jisoup@radcns.com>
2025-11-10 13:04:17 +0900

    CNAME 변경 진행

--- vite.config.ts
+++ vite.config.ts
@@ -9,7 +9,5 @@ export default defineConfig({
       '@': fileURLToPath(new URL('./src', import.meta.url)),
     },
   },
-  // GitHub Pages 리포지토리명에 맞게 수정
-  // 루트 도메인에 올리면 '/' 로 바꾸면 됨
   base: '/',
 })
